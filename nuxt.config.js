2d8aac50ad1183870546bc8307d0043815cb62cb
sn.chh89 <sn.chh89@gmail.com>
2021-05-14 00:41:31 +0700

    ssr true for facebook chat

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/vue-fb-customer-chat.js', ssr: false }
+    { src: '~/plugins/vue-fb-customer-chat.js', ssr: true }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
